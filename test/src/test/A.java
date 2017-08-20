5d4f964d0c8faee74d570d226140ca1b539c10da
cristiano <cristiano.mrmartins@gmail.com>
2017-08-20 13:46:29 -0300

    2.

--- test/src/test/A.java
+++ test/src/test/A.java
@@ -4,6 +4,8 @@ public class A {
 
 	public static void main(String[] args) {
 		B b = new B();
+		
+		b.setVal1(10);
 
 	}
 
--- test/src/test/B.java
+++ test/src/test/B.java
@@ -1,5 +1,28 @@
 package test;
 
 public class B {
-
+	
+	
+	private int val1;
+	
+	private int val2;
+	
+	
+	public int getVal1() {
+		return val1;
+	}
+	
+	public int getVal2() {
+		return val2;
+	}
+	
+	public void setVal1(int val1) {
+		this.val1 = val1;
+	}
+	
+	public void setVal2(int val2) {
+		this.val2 = val2;
+	}
+	
+	
 }
